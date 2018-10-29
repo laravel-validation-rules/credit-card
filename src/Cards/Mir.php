dd48253c08a1fb7897f42259da3e0d24ce2c6d07
esazykin <eugene.sazykin@gmail.com>
2018-10-29 14:45:17 +0300

    Mir Card (#12)
    * mir payment system
    
    * mir payment system

---
+++ src/Cards/Mir.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace LVR\CreditCard\Cards;
+
+use LVR\CreditCard\Contracts\CreditCard;
+
+/**
+ * Class Mir.
+ * @link https://nspk.com/
+ * @author eugene.sazykin@gmail.com
+ */
+class Mir extends Card implements CreditCard
+{
+    /**
+     * Regular expression for card number recognition.
+     *
+     * @var string
+     */
+    public static $pattern = '/^220/';
+
+    /**
+     * Credit card type.
+     *
+     * @var string
+     */
+    protected $type = 'credit';
+
+    /**
+     * Credit card name.
+     *
+     * @var string
+     */
+    protected $name = 'mir';
+
+    /**
+     * Brand name.
+     *
+     * @var string
+     */
+    protected $brand = 'Mir';
+
+    /**
+     * Card number length's.
+     *
+     * @var array
+     */
+    protected $number_length = [16];
+
+    /**
+     * CVC code length's.
+     *
+     * @var array
+     */
+    protected $cvc_length = [3];
+
+    /**
+     * Test cvc code checksum against Luhn algorithm.
+     *
+     * @var bool
+     */
+    protected $checksum_test = true;
+}
